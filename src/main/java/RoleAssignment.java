25f48e704641330de082007832ce3d8d5c7c3774
NailGad <nailgadirov2019@gmail.com>
2026-02-19 01:50:57 +0700

    1.5. Интерфейс назначения роли

---
+++ src/main/java/RoleAssignment.java
@@ -0,0 +1,17 @@
+public interface RoleAssignment {
+
+
+    String assignmentId();
+
+    User user();
+
+    Role role();
+
+    AssignmentMetadata metadata();
+
+
+    boolean isActive();
+
+    
+    String assignmentType();
+}
